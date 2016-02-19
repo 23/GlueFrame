aae79e061ed2df9cd891014c89eabc798abff4fa
Kalle Kabell <kkabell@gmail.com>
2016-02-19 14:16:43 +0100

    Make a few vars and methods private to the GlueFrame object

--- GlueFrame.js
+++ GlueFrame.js
@@ -6,161 +6,160 @@ var GlueFrame = function(iframe, appName) {
     $this.glueframe = "1.1.1";
 
     // Allow posting messages only to the domain of the app
-    $this.domain = (""+iframe.src).split("/").slice(0,3).join("/");
+    var _domain = (""+iframe.src).split("/").slice(0,3).join("/");
 
     // Determine method of communication with iframe
-    $this.getMethod = function() {
-        if ($this.domain == (""+window.location).split("/").slice(0,3).join("/") ) {
+    var _method = (function() {
+        if (_domain == (""+window.location).split("/").slice(0,3).join("/") ) {
             return "object";
         } else if (typeof window.postMessage !== "undefined") {
             return "post";
         } else {
             return "none";
         }
-    }
-    $this.method = $this.getMethod();
+    })();
 
     // Poll the iframe until the app is bootstrapped
     $this.ready = false;
-    $this.readyInterval = window.setInterval(function(){
-        if (!this.ready && $this.method === "object") {
+    var _readyInterval = window.setInterval(function(){
+        if (!this.ready && _method === "object") {
             if (iframe.contentWindow[appName] && iframe.contentWindow[appName].bootstrapped) {
                 $this.ready = true;
-                window.clearInterval($this.readyInterval);
-                $this.processQueue();
+                window.clearInterval(_readyInterval);
+                _processQueue();
             }
-        } else if (!this.ready && $this.method === "post") {
+        } else if (!this.ready && _method === "post") {
             $this.get("bootstrapped", function(bootstrapped){
                 if (bootstrapped) {
                     $this.ready = true;
-                    window.clearInterval($this.readyInterval);
-                    $this.processQueue();
+                    window.clearInterval(_readyInterval);
+                    _processQueue();
                 }
             }, true);
         }
     }, 100);
 
-    $this.callbackId = -1;
+    var _callbackId = -1;
     $this.glueFrameId = (new Date()).getTime();
-    $this.callbacks = {};
+    var _callbacks = {};
 
     // Store callback functions in the parent window
-    $this.registerCallback = function(callback, requireCallback) {
-        $this.callbackId = $this.callbackId + 1;
+    var _registerCallback = function(callback, requireCallback) {
+        _callbackId = _callbackId + 1;
         if (requireCallback && callback !== undefined && typeof callback === "function") {
-            $this.callbacks[$this.glueFrameId+"_"+$this.callbackId] = callback;
+            _callbacks[$this.glueFrameId+"_"+_callbackId] = callback;
         } else if (!requireCallback && (callback === undefined || typeof callback === "function")) {
-            $this.callbacks[$this.glueFrameId+"_"+$this.callbackId] = callback;
+            _callbacks[$this.glueFrameId+"_"+_callbackId] = callback;
         } else {
             throw "GlueFrame: Callback not registered correctly.";
         }
-        return $this.glueFrameId+"_"+$this.callbackId;
+        return $this.glueFrameId+"_"+_callbackId;
     };
 
     // Queue up method calls until app is ready
-    $this.queue = [];
-    $this.addToQueue = function(method, args) {
-        $this.queue.push({method: method, args: args});
+    var _queue = [];
+    var _addToQueue = function(method, args) {
+        _queue.push({method: method, args: args});
     };
 
     // Loop through queue when app is ready
-    $this.processQueue = function() {
-        for (var i = 0; i < $this.queue.length; i += 1) {
-            var queueItem = $this.queue[i];
+    var _processQueue = function() {
+        for (var i = 0; i < _queue.length; i += 1) {
+            var queueItem = _queue[i];
             queueItem.method.apply(null, queueItem.args);
         }
-        $this.queue = [];
+        _queue = [];
         $this.set("queuedEventsProcessed", true);
     };
 
     $this.get = function(prop, callback, force) {
         if (!$this.ready && !force) {
-            $this.addToQueue($this.get, [prop, callback]);
+            _addToQueue($this.get, [prop, callback]);
             return;
         }
-        var cbId = $this.registerCallback(callback, true);
-        if ($this.method === "object") {
+        var cbId = _registerCallback(callback, true);
+        if (_method === "object") {
             var value = iframe.contentWindow[appName].get.apply(null, [prop]);
-            if ($this.callbacks[$this.callbackId] !== undefined) {
-                $this.callbacks[$this.callbackId].apply(null, [value]);
+            if (_callbacks[_callbackId] !== undefined) {
+                _callbacks[_callbackId].apply(null, [value]);
             }
-        } else if ($this.method === "post") {
+        } else if (_method === "post") {
             var messageObject = {f: "get", args: [prop], cbId: cbId};
-            iframe.contentWindow.postMessage( JSON.stringify(messageObject), force ? "*" : $this.domain );
+            iframe.contentWindow.postMessage( JSON.stringify(messageObject), force ? "*" : _domain );
         }
     };
 
     $this.set = function(prop, val, callback) {
         if (!$this.ready) {
-            $this.addToQueue($this.set, [prop, val, callback]);
+            _addToQueue($this.set, [prop, val, callback]);
             return;
         }
-        var cbId = $this.registerCallback(callback, false);
-        if ($this.method === "object") {
+        var cbId = _registerCallback(callback, false);
+        if (_method === "object") {
             var value = iframe.contentWindow[appName].set.apply(null, [prop, val]);
-            if ($this.callbacks[$this.callbackId] !== undefined) {
-                $this.callbacks[$this.callbackId].apply(null, [value]);
+            if (_callbacks[_callbackId] !== undefined) {
+                _callbacks[_callbackId].apply(null, [value]);
             }
-        } else if ($this.method === "post") {
+        } else if (_method === "post") {
             var messageObject = {f: "set", args: [prop, val], cbId: cbId};
-            iframe.contentWindow.postMessage( JSON.stringify(messageObject), $this.domain );
+            iframe.contentWindow.postMessage( JSON.stringify(messageObject), _domain );
         }
     };
 
     $this.bind = function(event, callback, triggerQueue) {
         var triggerQueue = triggerQueue || false;
         if (!$this.ready) {
-            $this.addToQueue($this.bind, [event, callback, true]);
+            _addToQueue($this.bind, [event, callback, true]);
             return;
         }
-        var cbId = $this.registerCallback(callback, true);
-        if ($this.method === "object") {
+        var cbId = _registerCallback(callback, true);
+        if (_method === "object") {
             iframe.contentWindow[appName].bind.apply(null, [event, callback, triggerQueue]);
-        } else if ($this.method === "post") {
+        } else if (_method === "post") {
             var messageObject = {f: "bind", args: [event], cbId: cbId, triggerQueue: triggerQueue};
-            iframe.contentWindow.postMessage( JSON.stringify(messageObject), $this.domain );
+            iframe.contentWindow.postMessage( JSON.stringify(messageObject), _domain );
         }
     };
 
     $this.fire = function(event, obj) {
         if (!$this.ready) {
-            $this.addToQueue($this.fire, [event, obj]);
+            _addToQueue($this.fire, [event, obj]);
             return;
         }
-        if ($this.method === "object") {
+        if (_method === "object") {
             return iframe.contentWindow[appName].fire.apply(null, [event, obj]);
-        } else if ($this.method === "post") {
+        } else if (_method === "post") {
             var messageObject = {f: "fire", args: [event, obj]};
-            iframe.contentWindow.postMessage( JSON.stringify(messageObject), $this.domain );
+            iframe.contentWindow.postMessage( JSON.stringify(messageObject), _domain );
         }
     };
 
     // Remove event listeners, callbacks and intervals
     $this.destroy = function(){
         if (window.addEventListener) {
-            window.removeEventListener("message", $this.receiveMessage, false);
+            window.removeEventListener("message", _receiveMessage, false);
         } else {
-            window.detachEvent("onmessage", $this.receiveMessage);
+            window.detachEvent("onmessage", _receiveMessage);
         }
-        window.clearInterval($this.readyInterval);
-        $this.callbacks = {};
+        window.clearInterval(_readyInterval);
+        _callbacks = {};
     };
 
     // Parse messages received from iframe
-    $this.receiveMessage = function(e) {
-    	if (e.origin === $this.domain) {
+    var _receiveMessage = function(e) {
+    	if (e.origin === _domain) {
             var data = JSON.parse(e.data);
-            if (data.cbId !== undefined && $this.callbacks[data.cbId] !== undefined) {
-                $this.callbacks[data.cbId].apply(null, [data.a, data.b]);
+            if (data.cbId !== undefined && _callbacks[data.cbId] !== undefined) {
+                _callbacks[data.cbId].apply(null, [data.a, data.b]);
             }
         }
     };
 
     // Listen for message events if need
     if (window.addEventListener) {
-        window.addEventListener("message", $this.receiveMessage, false);
+        window.addEventListener("message", _receiveMessage, false);
     } else {
-        window.attachEvent("onmessage", $this.receiveMessage);
+        window.attachEvent("onmessage", _receiveMessage);
     }
 
 };
